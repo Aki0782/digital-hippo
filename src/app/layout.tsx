7f7965220ef617ccdd240393c6b239a6fc0b4246
Akhil Aravindakshan <akhil.dev.00782@gmail.com>
2023-12-31 16:45:11 -0400

    add navbar component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { cn } from "../lib/utils";
 
 import "./globals.css";
 
+import Navbar from "@/components/Navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +21,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
+          <Navbar />
           <div className="flex-grow flex-1">{children}</div>
         </main>
       </body>
